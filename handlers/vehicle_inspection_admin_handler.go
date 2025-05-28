d8b3ecd31f16426556880a9a3175b5a1826b0aef
Plerm Inchuay <plerm@talonnet.co.th>
2025-05-29 00:33:24 +0700

    update 2025-05-28(2)

--- handlers/vehicle_inspection_admin_handler.go
+++ handlers/vehicle_inspection_admin_handler.go
@@ -70,7 +70,7 @@ func (h *VehicleInspectionAdminHandler) SearchRequests(c *gin.Context) {
 	query := h.SetQueryRole(user, config.DB)
 	query = query.Table("public.vms_trn_request").
 		Select("vms_trn_request.*, v.vehicle_license_plate,v.vehicle_license_plate_province_short,v.vehicle_license_plate_province_full,"+
-			"(select parking_place from vms_mas_vehicle_department d where d.mas_vehicle_uid = vms_trn_request.mas_vehicle_uid) parking_place ").
+			"(select max(parking_place) from vms_mas_vehicle_department d where d.mas_vehicle_uid = vms_trn_request.mas_vehicle_uid) parking_place ").
 		Joins("LEFT JOIN vms_mas_vehicle v on v.mas_vehicle_uid = vms_trn_request.mas_vehicle_uid").
 		Where("vms_trn_request.ref_request_status_code IN (?)", statusCodes)
 
