251036fcfc5707321405335ebe0c1608d5c2e4bb
Plermjai Inchuay <plerm@talonnet.co.th>
2025-07-09 14:42:22 +0000

    Update time_with_zone.go

--- models/time_with_zone.go
+++ models/time_with_zone.go
@@ -87,6 +87,7 @@ func (t TimeWithZone) MarshalJSON() ([]byte, error) {
 
 	// Format as RFC3339 with +07:00 timezone
 	formatted := convertedTime.Format("2006-01-02T15:04:05-07:00")
+	formatted = strings.ReplaceAll(formatted, "+07:00", "Z")
 	return json.Marshal(formatted)
 }
 
