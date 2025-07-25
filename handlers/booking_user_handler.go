4bdc75a52c9810962e65fe3a75cebb1bc0a82816
Plermjai Inchuay <plerm@talonnet.co.th>
2025-07-25 09:49:44 +0000

    Update file booking_user_handler.go

--- handlers/booking_user_handler.go
+++ handlers/booking_user_handler.go
@@ -397,6 +397,12 @@ func (h *BookingUserHandler) SearchRequests(c *gin.Context) {
 	}
 	for i := range requests {
 		requests[i].RefRequestStatusName = statusNameMap[requests[i].RefRequestStatusCode]
+		requests[i].CanScoreButton = funcs.IsAllowScoreButton(requests[i].TrnRequestUid)
+		if requests[i].CanScoreButton {
+			requests[i].CanPickupButton = false
+		} else {
+			requests[i].CanPickupButton = funcs.IsAllowPickupButton(requests[i].TrnRequestUid)
+		}
 	}
 
 	// Build the summary query
